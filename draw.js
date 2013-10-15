d8149dc68a500dce7d9732adf759370a8a5f9a9f
Apprentice <apprentice@dbc12.local>
2013-10-15 16:19:09 -0700

    Added Save, clear, and restore functionality

--- draw.js
+++ draw.js
@@ -1,41 +1,42 @@
-function draw(x,y) {
-  var ctx = document.getElementById("a").getContext("2d");
-  ctx.fillRect(x,y,100,100);
-
-};
-
-var x = 20;
-var y = 20;
-
-// document.getElementById("a").addEventListener("click", function(){
-//   draw(x,y);
-//   x += 5;
-//   y += 5;
-// });
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var save = document.getElementById("Save");
+var restore = document.getElementById("Restore");
+var clear = document.getElementById("clear");
 
 function drawing_dot(x,y){
-  var ctx = document.getElementById("a").getContext("2d");
   ctx.fillRect(x,y,5,5);
 }
 
-// document.getElementById("a").addEventListener("mousemove",function(e){
-//   drawing_dot(e.x,e.y);
-// });
-
-
-//add mousedown -> add mousemove and add mouseup( -- will kill current events and go back to mousedown)
-var canvas = document.getElementById("a");
 
 var flag = 0;
+
 canvas.addEventListener("mousedown",function(){
   flag = 1;
 });
+
 canvas.addEventListener("mouseup", function(){
   flag = 0;
 });
+
 canvas.addEventListener("mousemove", function(e){
     if (flag === 1) {
-
-      drawing_dot(e.x,e.y);
+      drawing_dot(e.offsetX,e.offsetY);
     };
+});
+
+save.addEventListener("click", function () {
+  dataURL = canvas.toDataURL();
+});
+
+clear.addEventListener("click", function () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+restore.addEventListener("click", function() {
+  var image = new Image();
+  image.onload=function() {
+    ctx.drawImage(image,0,0);
+  }
+  image.src = dataURL;
 });
