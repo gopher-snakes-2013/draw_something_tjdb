1efb191ea08551b91bbed5b99c4d9e5f313aa05c
Jeff Edwards <js_edwards@yahoo.com>
2013-10-16 06:25:52 -0700

    updated database url env variable in environment file

--- environment.rb
+++ environment.rb
@@ -1,11 +1,26 @@
+
+ENV['DATABASE_URL']='postgres://localhost/draw_kitty'
 configure :production, :development, :test do 
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/draw_kitty')
+
+  # puts "host:"
+  # puts db.host
+  # puts "adapter:"
+  # puts db.adapter
+  # puts "username:"
+  # put db.username
+  # puts "password"
+  # puts db.password
+  # puts "database"
+  # puts db.database 
+  # puts "DATABASE_URL"
+  # puts ENV['DATABASE_URL']
 
   ActiveRecord::Base.establish_connection(
       adapter: 'postgresql',
       host: db.host,
       username: db.user,
-      password: db.password,
+      password: "",
       database: db.path[1..-1],
       encoding: 'utf8'
 
