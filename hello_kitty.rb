eb0ff1373e540877d9ed08529f999e48a3bbd95d
Jeff Edwards <js_edwards@yahoo.com>
2013-10-16 16:07:11 -0700

    remove set database line from hello_kitty

--- hello_kitty.rb
+++ hello_kitty.rb
@@ -5,7 +5,6 @@ require './models/drawing'
 use Rack::MethodOverride
 
 
-# set :database, ENV['DATABASE_URL'] || 'postgres:://localhost/draw_kitty'
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/draw_kitty')
 
 get '/' do
