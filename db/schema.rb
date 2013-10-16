82d9ae3ac811e84412c62bb78cb29af455e4bd4e
Jeff Edwards <js_edwards@yahoo.com>
2013-10-16 16:54:07 -0700

    updated drawings table dataURL field to be text to store longer strings

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131015230224) do
+ActiveRecord::Schema.define(version: 20131016234929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "drawings", force: true do |t|
-    t.string   "dataURL"
+    t.text     "dataURL"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
