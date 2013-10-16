aa84abea1bc64b7b6325eca2f6a50c633a62b33d
Jeff Edwards <js_edwards@yahoo.com>
2013-10-16 13:53:13 -0700

    simplify activerecord db connection

--- environment.rb
+++ environment.rb
@@ -1,28 +1,20 @@
 
-ENV['DATABASE_URL']='postgres://localhost/draw_kitty'
-configure :production, :development, :test do 
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/draw_kitty')
+# ENV['DATABASE_URL']='postgres://localhost/draw_kitty'
+# configure :production, :development, :test do 
+#   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/draw_kitty')
 
-  # puts "host:"
-  # puts db.host
-  # puts "adapter:"
-  # puts db.adapter
-  # puts "username:"
-  # put db.username
-  # puts "password"
-  # puts db.password
-  # puts "database"
-  # puts db.database 
-  # puts "DATABASE_URL"
-  # puts ENV['DATABASE_URL']
+#   # puts "host:"
+#   # puts db.host
+#   # puts "adapter:"
+#   # puts db.adapter
+#   # puts "username:"
+#   # put db.username
+#   # puts "password"
+#   # puts db.password
+#   # puts "database"
+#   # puts db.database 
+#   # puts "DATABASE_URL"
+#   # puts ENV['DATABASE_URL']
 
-  ActiveRecord::Base.establish_connection(
-      adapter: 'postgresql',
-      host: 'localhost',
-      username: db.user,
-      password: "",
-      database: 'draw_kitty',
-      # encoding: 'utf8'
-
-    )
-end
+#   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/draw_kitty')
+# end
--- hello_kitty.rb
+++ hello_kitty.rb
@@ -2,12 +2,13 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environment'
 require './dotenv'
+# require 'activerecord'
 
 use Rack::MethodOverride
 
 
-set :database, ENV['DATABASE_URL'] || 'postgres:://localhost/draw_kitty'
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+# set :database, ENV['DATABASE_URL'] || 'postgres:://localhost/draw_kitty'
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/draw_kitty')
 
 get '/' do
   erb :index
