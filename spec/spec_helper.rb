bc87726abf279d7c799e8020b41299c1a35289da
salarkhan <salar.k3@gmail.com>
2013-10-17 15:43:35 -0700

    Added to spec_helper to destroy Comment and Drawing DB entries before running tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,10 @@ require 'capybara/rspec'
 require 'shoulda-matchers'
 
 Capybara.app = Sinatra::Application
+
+RSpec.configure do |config|
+  config.before do
+    Comment.destroy_all
+    Drawing.destroy_all
+  end
+end
